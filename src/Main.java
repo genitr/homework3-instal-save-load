8b47ccd850776e8db7e874b2666946f95cc1b580
Юрий Данильченко <genitra111@gmail.com>
2022-06-01 15:30:52 +1000

    законченное домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ package src;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
-    public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder();
+    private static String LOG_PATH;
+    private static final StringBuilder GAME_LOG = new StringBuilder();
+
+    public static void main(String[] args) {
 
         // список директорий
         List<File> gameDirectories = new ArrayList<>();
@@ -25,15 +26,16 @@ public class Main {
         gameDirectories.add(new File("C:\\Games\\res\\vectors"));
         gameDirectories.add(new File("C:\\Games\\res\\icons"));
 
-        final String save_Dir = String.valueOf(gameDirectories.get(2));
+        String saveDir = String.valueOf(gameDirectories.get(2));
 
         // список файлов
         List<File> gameFiles = new ArrayList<>();
         gameFiles.add(new File("C:\\Games\\src\\main\\Main.java"));
         gameFiles.add(new File("C:\\Games\\src\\main\\Utils.java"));
         gameFiles.add(new File("C:\\Games\\temp\\temp.txt"));
+        LOG_PATH = String.valueOf(gameFiles.get(2));
 
-        gameInstall(gameDirectories, gameFiles, sb);
+        gameInstall(gameDirectories, gameFiles);
 
         List<GameProgress> gameProgress = new ArrayList<>();
         gameProgress.add(new GameProgress(110, 2, 1, 125.6));
@@ -43,22 +45,19 @@ public class Main {
         List<String> saveList = new ArrayList<>();
 
         // Сохранение
-        saveGame(save_Dir, gameProgress, saveList);
-
-        zipFiles(save_Dir, saveList);
+        saveGame(saveDir, gameProgress, saveList);
+        zipFiles(saveDir, saveList);
 
         // Загрузка
-        openZip(save_Dir, save_Dir);
-
-        openProgress(save_Dir, gameProgress.get(0));
-
-        System.out.println(gameProgress.get(0));
+        openZip(saveDir, saveDir);
+        GameProgress loadProgress = openProgress(saveDir);
+        System.out.println(loadProgress);
     }
 
-    public static void createDirectory(File dir, StringBuilder gameLog) {
+    public static void createDirectory(File dir) {
         if(dir.mkdir()) {
             System.out.println("Директория была создана");
-            gameLog.append("Создана папка ~ ")
+            GAME_LOG.append("Создана папка ~ ")
                     .append(dir.getName())
                     .append(" [")
                     .append(dir.getPath())
@@ -67,11 +66,11 @@ public class Main {
         }
     }
 
-    public static void createFile(File gameFile, StringBuilder gameLog) {
+    public static void createFile(File gameFile) {
         try {
             if (gameFile.createNewFile()) {
                 System.out.println("Файл был создан");
-                gameLog.append("Создан файл ~ ")
+                GAME_LOG.append("Создан файл ~ ")
                         .append(gameFile.getName())
                         .append(" [")
                         .append(gameFile.getPath())
@@ -83,44 +82,43 @@ public class Main {
         }
     }
 
-    public static void gameInstall (List<File> directories, List<File> files, StringBuilder sb) {
+    public static void gameInstall (List<File> directories, List<File> files) {
         for (File gameDirectory : directories) {
-            createDirectory(gameDirectory, sb);
+            createDirectory(gameDirectory);
         }
 
         for (File gameFile : files) {
-            createFile(gameFile, sb);
+            createFile(gameFile);
         }
-        createLog(sb);
+        createLog();
     }
 
-    public static void createLog(StringBuilder sb) {
-        String gameLog = sb.toString();
-        try(FileWriter log = new FileWriter("C:\\Games\\temp\\temp.txt");) {
-            log.write(gameLog);
+    public static void createLog() {
+        try(FileWriter logFile = new FileWriter(LOG_PATH)) {
+            logFile.write(GAME_LOG.toString());
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
     public static void saveGame(String path, List<GameProgress> gameProgress, List<String> saveList) {
-        String name = "\\save";
+        //String name = "\\save";
         for (int i = 0; i < gameProgress.size(); i++) {
-            try(FileOutputStream fos = new FileOutputStream(path + name + (i + 1) + ".dat");
+            try(FileOutputStream fos = new FileOutputStream(path + "\\save" + (i + 1) + ".dat");
                 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
                 oos.writeObject(gameProgress.get(i));
                 } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
-            saveList.add(path + "\\" + name + (i + 1) + ".dat");
+            saveList.add(path + "\\save" + (i + 1) + ".dat");
         }
     }
 
-    public static void zipFiles(String path, List<String> list) {
+    public static void zipFiles(String path, List<String> saveList) {
         String name = "\\save.zip";
         try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path + name))) {
-            for (int i = 0; i < list.size(); i++) {
-                try(FileInputStream fis = new FileInputStream(list.get(i));) {
+            for (int i = 0; i < saveList.size(); i++) {
+                try(FileInputStream fis = new FileInputStream(saveList.get(i))) {
                     ZipEntry entry = new ZipEntry("save" + (i + 1) + ".dat");
                     zout.putNextEntry(entry);
                     byte[] buffer = new byte[fis.available()];
@@ -130,7 +128,7 @@ public class Main {
                 } catch (Exception ex) {
                     System.out.println(ex.getMessage());
                 }
-                File fileToDelete = new File(list.get(i));
+                File fileToDelete = new File(saveList.get(i));
                 if (fileToDelete.exists()) fileToDelete.delete();
                 else System.out.println("Файд не найден");
             }
@@ -160,9 +158,10 @@ public class Main {
         }
     }
 
-    public static GameProgress openProgress(String path, GameProgress progress) {
-        String fileName = "\\save.dat";
-        try(FileInputStream fis = new FileInputStream(path)) {
+    public static GameProgress openProgress(String path) {
+        GameProgress progress = null;
+        String fileName = "\\save3.dat";
+        try(FileInputStream fis = new FileInputStream(path + fileName)) {
             ObjectInputStream ois = new ObjectInputStream(fis);
             progress = (GameProgress) ois.readObject();
         } catch (Exception ex) {
@@ -170,5 +169,4 @@ public class Main {
         }
         return progress;
     }
-
 }
